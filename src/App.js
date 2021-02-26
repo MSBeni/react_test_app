c393c45577e0d021f06312c79260f782729e7d3b
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-26 16:37:20 -0500

    Added some dynamic files to the person 2 added children 4

--- src/App.js
+++ src/App.js
@@ -1,36 +1,58 @@
-// import logo from './logo.svg';
+
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi I am a React App</h1>
-      <p>This is really working ...</p>
-      <Person name="Noushi" age="30"/>
-      <Person name="Mmammali" age="30">My Hobbies: Books</Person>
-      <Person name="Mahda" age="8" />
-    </div>
-  );
-  // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does it works now??'));
+
+class App extends Component {
+  state = {
+    persons: [
+      {name:"Noush Gol", age:"31"},
+      {name:"MiMI", age:"31"},
+      {name:"Faraz", age:"5"},
+    ]
+  }
+  render(){
+    return(
+      <div className="App">
+        <h1>Hi I am a React App</h1>
+        <p>This is really working ...</p>
+        <button>Switch Name</button>
+        <Person name="Noushi" age="30"/>
+        <Person name="Mmammali" age="30">My Hobbies: Books</Person>
+        <Person name="Mahda" age="8" />
+      </div>
+    )
+  }
 }
 
 export default App;
 
-
+// import logo from './logo.svg';
 // import React, { Component } from 'react';
 // import './App.css';
+// import Person from './Person/Person';
 
-
-// class App extends Component {
-//   render(){
-//     return(
-//       <div>
-//         <h1>Hi, I am a React App</h1>
-//       </div>
-//     )
-//   }
+// function App() {
+  // state = {
+  //   persons: [
+  //     {name:"Noush Gol", age:"31"},
+  //     {name:"MiMI", age:"31"},
+  //     {name:"Faraz", age:"5"},
+  //   ]
+  // }
+//   return (
+//     <div className="App">
+      // <h1>Hi I am a React App</h1>
+      // <p>This is really working ...</p>
+      // <button>Switch Name</button>
+      // <Person name="Noushi" age="30"/>
+      // <Person name="Mmammali" age="30">My Hobbies: Books</Person>
+      // <Person name="Mahda" age="8" />
+//     </div>
+//   );
+//   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does it works now??'));
 // }
 
 // export default App;
+
