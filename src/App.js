b90471bbeed515e4ab93b4c74beecdc2b107e5bb
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-26 16:16:16 -0500

    Added some dynamic files to the person

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
       <h1>Hi I am a React App</h1>
       <p>This is really working ...</p>
       <Person />
+      <Person />
+      <Person />
     </div>
   );
   // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does it works now??'));
