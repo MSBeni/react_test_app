eb58aaef3a6422c4a946b27aba757503ae493181
MSBeni <andrei.sokurov.bitco@gmail.com>
2021-02-26 16:25:00 -0500

    Added some dynamic files to the person 2 added children

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,7 +1,13 @@
 import React from 'react'
 
-const person = () => {
-    return <p> I am a person and I am {Math.floor(Math.random() * 30)} </p>
+const person = (props) => {
+    return (
+        <div>
+            <p> I am {props.name} and I am {props.age} years old</p>
+            <p>{props.children}</p>
+        </div>
+    
+    )
 };
 
 export default person;
